392a6a9a3d915bfb86e53d3b0e540dc5845f243e
Joe Winter <jt.winter8@gmail.com>
2024-09-08 00:10:33 +0100

    write code

---
+++ main.py
@@ -0,0 +1,29 @@
+# fruits = ["apple", "banana", "cherry", "peach", "mango"]
+
+# print(fruits)
+
+# for number in fruits:
+#     print(number)
+
+# count = 10
+# while count <= 20:
+#     print (count)
+#     count += 1
+
+# def multiply_x_5 (number):
+#     answer = number * 5
+#     print (answer)
+
+# count = 1
+# while count <= 10:
+#     multiply_x_5 (count)
+#     count +=1
+
+# def add(a, b):
+#     return a + b
+
+# result = add(99, 9999999999999)
+# print (result)
+
+# result = 10 / 0
+# print(result)
